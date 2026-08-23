55029460af2462f27d94f7cb35587b572df72e70
Arianna Sala <ari.sala@live.com>
2026-08-23 17:09:01 +0200

    avoid showing in generation graphs glitches (really quick drops in generation)

--- src/etl/read_data_from_oracle.py
+++ src/etl/read_data_from_oracle.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 
+MINIMUM_GENERATION_DERIVATIVE = 15000
 
 def read_data_from_oracle(connection, table_name, date, country):
     sql = f"""
@@ -34,6 +36,42 @@ def get_available_countries(connection, table_name):
 
     return available_countries
 
+def _transform_generation_data(raw_generation):
+    actual_consumption_indices = raw_generation[
+            raw_generation["GENERATION_TYPE"] == "Actual Consumption"
+        ].index
+        
+    generation = raw_generation.copy()
+
+    generation.loc[actual_consumption_indices, "GENERATION"] = (
+        generation.loc[actual_consumption_indices, "GENERATION"] * -1
+    )
+    generation["SOURCE"] = (
+        generation["GENERATION_TYPE"] + " " + generation["GENERATION_SOURCE"]
+    )
+    generation["SOURCE"] = generation["SOURCE"].str.replace(
+        "Actual Aggregated ", "", regex=False
+    )
+
+    generation_pivot = generation.pivot(
+        index="TIMESTAMP", columns="SOURCE", values="GENERATION"
+    ).reset_index()
+
+    # we check the derivative to fill very short glitches in the data
+    derivative = generation_pivot.drop(columns = ["TIMESTAMP"]).sum(axis=1).diff()
+    is_glitch = (derivative < - MINIMUM_GENERATION_DERIVATIVE) & (
+        derivative.shift(-1) > MINIMUM_GENERATION_DERIVATIVE)
+
+    generation_pivot.loc[is_glitch, generation_pivot.columns] = np.nan
+    generation_pivot = generation_pivot.interpolate()
+
+    columns_zero_sum = generation_pivot.drop(columns=["TIMESTAMP"]).columns[
+            generation_pivot.drop(columns=["TIMESTAMP"]).sum(axis=0) == 0
+    ]
+    generation_pivot = generation_pivot.drop(columns=columns_zero_sum)
+
+    return generation_pivot
+
 
 def get_and_trasform_data(connection, selected_date, selected_country):
     load = read_data_from_oracle(
@@ -45,25 +83,8 @@ def get_and_trasform_data(connection, selected_date, selected_country):
     generation = read_data_from_oracle(
         connection, "generation_raw", selected_date, selected_country
     )
-    actual_consumption_indices = generation[
-        generation["GENERATION_TYPE"] == "Actual Consumption"
-    ].index
-    generation.loc[actual_consumption_indices, "GENERATION"] = (
-        generation.loc[actual_consumption_indices, "GENERATION"] * -1
-    )
-    generation["SOURCE"] = (
-        generation["GENERATION_TYPE"] + " " + generation["GENERATION_SOURCE"]
-    )
-    generation["SOURCE"] = generation["SOURCE"].str.replace(
-        "Actual Aggregated", "", regex=False
-    )
 
-    generation_pivot = generation.pivot(
-        index="TIMESTAMP", columns="SOURCE", values="GENERATION"
-    ).reset_index()
-    columns_zero_sum = generation_pivot.drop(columns=["TIMESTAMP"]).columns[
-        generation_pivot.drop(columns=["TIMESTAMP"]).sum(axis=0) == 0
-    ]
-    generation_pivot = generation_pivot.drop(columns=columns_zero_sum)
+    generation_pivot = _transform_generation_data(generation)
+    
 
-    return load, day_ahead_prices, generation_pivot
+    return load, day_ahead_prices, generation_pivot    
